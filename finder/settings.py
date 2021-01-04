763b74bb7c3e82b487d96f3cb4d59cc2efb8e4fd
Axell Concha Regatto <aconcha@ecuaquimica.com.ec>
2021-01-04 17:43:30 -0500

    static root fix5

--- finder/settings.py
+++ finder/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-from pathlib import Path
 
+#cd ~/kaconcha.pythonanywhere.com
+#python3.8 -i /var/www/kaconcha_pythonanywhere_com_wsgi.py
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
